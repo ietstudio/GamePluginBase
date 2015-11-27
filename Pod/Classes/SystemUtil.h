933111f0f3036f21c6bf8c6cfafac7b64a3d980a
gaoyang <geekgaoyang@gmail.com>
2015-11-27 11:36:51 +0800

    update to 0.1.3

--- Pod/Classes/SystemUtil.h
+++ Pod/Classes/SystemUtil.h
@@ -9,32 +9,17 @@
 #import <Foundation/Foundation.h>
 #import "Macros.h"
 
+// 配置信息
+#define IETCONFIGS_KEY                      @"IETConfigs"
+// Apple ID
 #define APPLE_ID                            @"Apple_ID"
 
 @interface SystemUtil : NSObject
 
+@property(nonatomic, retain)UIWindow* window;
+@property(nonatomic, retain)UIViewController* controller;
+
 SINGLETON_DECLARE(SystemUtil)
-
-/**
- *  获取国家代码，获取不到返回US
- *
- *  @return 国家代码 eg.US
- */
-- (NSString*)getCountryCode;
-
-/**
- *  获取当前的window
- *
- *  @return window
- */
-- (UIWindow*)getCurrentWindow;
-
-/**
- *  获取当前的ViewController
- *
- *  @return UIViewController
- */
-- (UIViewController*)getCurrentViewController;
 
 /**
  *  获取info.plist中的配置信息
--- Pod/Classes/SystemUtil.m
+++ Pod/Classes/SystemUtil.m
@@ -8,51 +8,11 @@
 
 #import "SystemUtil.h"
 
-// 默认国家
-#define DEFAULT_COUNTRY_CODE @"US"
-
-// 配置信息
-#define IETCONFIGS_KEY                      @"IETConfigs"
-
 @implementation SystemUtil
 
 SINGLETON_DEFINITION(SystemUtil)
 
-#pragma mark - private
-
-- (UIViewController *)topViewController:(UIViewController *)rootViewController
-{
-    if (rootViewController.presentedViewController == nil) {
-        return rootViewController;
-    }
-    if ([rootViewController.presentedViewController isMemberOfClass:[UINavigationController class]]) {
-        UINavigationController *navigationController = (UINavigationController *)rootViewController.presentedViewController;
-        UIViewController *lastViewController = [[navigationController viewControllers] lastObject];
-        return [self topViewController:lastViewController];
-    }
-    UIViewController *presentedViewController = (UIViewController *)rootViewController.presentedViewController;
-    return [self topViewController:presentedViewController];
-}
-
 #pragma mark - public
-
-- (NSString *)getCountryCode {
-    NSString *country = [[NSLocale currentLocale] objectForKey: NSLocaleCountryCode];
-    if(!country) {
-        country = [[NSLocale systemLocale] objectForKey:NSLocaleCountryCode];
-    }
-    country = country==nil ? DEFAULT_COUNTRY_CODE : country;
-    return country;
-}
-
-- (UIWindow *)getCurrentWindow {
-    UIWindow *window = [[UIApplication sharedApplication] keyWindow];
-    return window;
-}
-
-- (UIViewController *)getCurrentViewController {
-    return [self topViewController:[UIApplication sharedApplication].keyWindow.rootViewController];
-}
 
 - (NSString *)getConfigValueWithKey:(NSString *)key {
     NSDictionary* configs = [[NSBundle mainBundle] objectForInfoDictionaryKey:IETCONFIGS_KEY];
